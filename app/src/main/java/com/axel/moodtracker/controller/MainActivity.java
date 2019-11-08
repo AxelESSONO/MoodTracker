eb36c43c5b9d2dd4fa8ebbebd1bb8f615c387003
Axel ESSONO <naxelcdric@gmail>
2019-11-08 23:29:23 +0100

    I create a controller folder: MainActivity is in this folder

--- app/src/main/java/com/axel/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/axel/moodtracker/controller/MainActivity.java
@@ -1,8 +1,10 @@
-package com.axel.moodtracker;
+package com.axel.moodtracker.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.axel.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
