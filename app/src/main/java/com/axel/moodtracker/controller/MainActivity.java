13eea5023f1ab6ac15ca0083d633452a13765991
Axel ESSONO <naxelcdric@gmail>
2019-11-09 15:29:22 +0100

    - Setting icone of MoodTracker - Implementing MainActivity, if user clickd on imageView, the MoodActivity starts

--- app/src/main/java/com/axel/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/axel/moodtracker/controller/MainActivity.java
@@ -1,16 +1,28 @@
 package com.axel.moodtracker.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageView;
 import com.axel.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.happySmiley);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moodActivity = new Intent(MainActivity.this, MoodActivity.class);
+                startActivity(moodActivity);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/axel/moodtracker/controller/MoodActivity.java
@@ -0,0 +1,16 @@
+package com.axel.moodtracker.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.axel.moodtracker.R;
+
+public class MoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood);
+    }
+}
