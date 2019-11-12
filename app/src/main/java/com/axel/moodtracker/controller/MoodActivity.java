76f1425408c7bc98d3c6887547a868645b5010bb
Axel ESSONO <naxelcdric@gmail>
2019-11-12 23:21:10 +0100

    Added a data base SQLite to save comments by creating the classes :EditDataActivity, HistoryActivity and DataHelper. I also created the layout files: activity_history.xml, edit_data_layout.

--- app/src/main/java/com/axel/moodtracker/controller/MoodActivity.java
+++ app/src/main/java/com/axel/moodtracker/controller/MoodActivity.java
@@ -2,6 +2,8 @@ package com.axel.moodtracker.controller;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.axel.moodtracker.R;
 import com.axel.moodtracker.adapters.IconItemAdapter;
+import com.axel.moodtracker.model.DatabaseHelper;
 import com.axel.moodtracker.model.IconeItem;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +22,9 @@ import java.util.List;
 public class MoodActivity extends AppCompatActivity {
 
     private MoodActivity moodActivity;
+    public static final String TAG = "MoodActivity";
+    DatabaseHelper mDatabaseHelper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,7 @@ public class MoodActivity extends AppCompatActivity {
 
         // the popup to write the coment
         final ImageView myPopup = findViewById(R.id.add_comment_button);
+        mDatabaseHelper = new DatabaseHelper(this);
         myPopup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
@@ -59,9 +66,12 @@ public class MoodActivity extends AppCompatActivity {
                 mValidateBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        String newComment = mComent.getText().toString();
                         // Check if field is not empty
                         if(!mComent.getText().toString().isEmpty()){
-                            Toast.makeText(MoodActivity.this,"Votre commentaire est enrégistré avec succès",Toast.LENGTH_SHORT).show();
+                            addData(newComment);
+                            mComent.setText("");
+                            //Toast.makeText(MoodActivity.this,"Votre commentaire a été enrégistré avec succès",Toast.LENGTH_SHORT).show();
                             dialog.dismiss();
                         } else {
                             Toast.makeText(MoodActivity.this, "Veuillez saisir un commentaire s'il vous plaît !", Toast.LENGTH_SHORT).show();
@@ -78,5 +88,33 @@ public class MoodActivity extends AppCompatActivity {
                 });
             }
         });
+
+        final ImageView goToHistoryViewBtn = findViewById(R.id.add_history_button);
+        goToHistoryViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent hystoryActivity = new Intent(MoodActivity.this, HistoryActivity.class);
+                startActivity(hystoryActivity);
+            }
+        });
+    }
+
+    public void addData(String newEntry){
+        boolean inserData = mDatabaseHelper.addData(newEntry);
+
+        if(inserData){
+            toastMessage("Commentaire enregistré avec succès");
+        } else {
+            toastMessage("Votre commentaire n'a pu être enregistré, veuiller ressayer");
+        }
+    }
+
+    /**
+     * customizable message
+     * @param message
+     */
+
+    public void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 }
