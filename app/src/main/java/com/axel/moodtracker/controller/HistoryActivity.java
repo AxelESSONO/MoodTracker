b2841a4fbef76c5acec7f79608946b948fef2bd8
Axel ESSONO <naxelcdric@gmail>
2020-01-10 21:35:59 +0100

    Set width according to the mood.

--- app/src/main/java/com/axel/moodtracker/controller/HistoryActivity.java
+++ app/src/main/java/com/axel/moodtracker/controller/HistoryActivity.java
@@ -1,17 +1,21 @@
 package com.axel.moodtracker.controller;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.SimpleCursorAdapter;
@@ -34,10 +38,8 @@ public class HistoryActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-
         dbHelper = new MoodDbAdapter(this);
         dbHelper.open();
-
         //Clean all data
         //dbHelper.deleteAllMood();
         //Generate ListView from SQLite Database
@@ -70,26 +72,6 @@ public class HistoryActivity extends AppCompatActivity
         ListView listView = (ListView) findViewById(R.id.listView1);
         // Assign adapter to ListView
         listView.setAdapter(myCursorAdapter );
-        //long a = listView.getItemIdAtPosition(0);
-        //dataAdapter.setViewResource(Color.parseColor("AB1A49"));
-        listView.setOnItemClickListener(new OnItemClickListener()
-         {
-             @Override
-             public void onItemClick(AdapterView listView, View view,int position, long id)
-             {
-                 //int value = to[position];
-                // TextView txtC;
-                 //txtC = (TextView) findViewById(R.id.my_comment);
-                // String comment = txtC.getText().toString();
-
-                 //Toast.makeText(HistoryActivity.this, "J'affiche:  " + commentToShare, Toast.LENGTH_SHORT).show();
-
-                 //Intent intent = new Intent(getApplicationContext(), ContactsActivity.class);
-                 //intent.putExtra("edittext", comment);
-                 //intent.putExtra("nbLettre", nbLettre);
-                 //startActivity(intent);
-             }
-         });
     }
 
     //extend the SimpleCursorAdapter to create a custom class where we
@@ -101,11 +83,10 @@ public class HistoryActivity extends AppCompatActivity
         {
             super(context, layout, c, from, to, flags);
         }
-
         @Override
         public View getView(int position, View convertView, ViewGroup parent)
         {
-
+            // ============================= get width of the screen ==========================================
             //get reference to the row
             View view = super.getView(position, convertView, parent);
 
@@ -115,29 +96,27 @@ public class HistoryActivity extends AppCompatActivity
             ListView listView = (ListView) findViewById(R.id.listView1);
             Cursor cursor = (Cursor) listView.getItemAtPosition(position);
             // Get the state's capital from this row in the database.
-            String moodColor = cursor.getString(cursor.getColumnIndexOrThrow("color"));
+            final String moodColor = cursor.getString(cursor.getColumnIndexOrThrow("color"));
             final String moodComment = cursor.getString(cursor.getColumnIndexOrThrow("comment"));
 
+            view.setLayoutParams(new LinearLayout.LayoutParams(resizeWidthAccordingToMood(moodColor), measureHeight()/7));
             relativeLayout.setOnClickListener(new View.OnClickListener()
             {
                 @Override
                 public void onClick(View v)
                 {
-                    Intent intent = new Intent(getApplicationContext(), ContactsActivity.class);
+                    Intent intent = new Intent(HistoryActivity.this, ContactsActivity.class);
                     intent.putExtra("myComment", moodComment );
-                    //intent.putExtra("nbLettre", nbLettre);
                     startActivity(intent);
-                    //Toast.makeText(HistoryActivity.this, moodComment, Toast.LENGTH_SHORT).show();
                 }
             });
 
             //check for odd or even to set alternate colors to the row background
             view.setBackgroundColor(Color.parseColor(moodColor));
 
-            if (moodComment != "")
+            if (!moodComment.equals(""))
             {
                 imageView.setVisibility(View.VISIBLE);
-
                 imageView.setOnClickListener(new View.OnClickListener()
                 {
                     @Override
@@ -146,11 +125,74 @@ public class HistoryActivity extends AppCompatActivity
                         Toast.makeText(HistoryActivity.this, moodComment, Toast.LENGTH_SHORT).show();
                     }
                 });
-
-            } else {
-                imageView.setVisibility(View.GONE);
+            }
+            else {
+                //imageView.setVisibility(View.INVISIBLE);
+                Toast.makeText(HistoryActivity.this, "Pas de commentaire", Toast.LENGTH_SHORT).show();
             }
             return view;
         }
+    }
+
+    private int measureWidth()
+    {
+        WindowManager wm = (WindowManager) this.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        int screenWidth;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2)
+        {
+            Point size = new Point();
+            display.getSize(size);
+            screenWidth = size.x;
+        }
+        else
+        {
+            screenWidth = display.getWidth();
+        }
+        return screenWidth;
+    }
+
+    private int measureHeight()
+    {
+        WindowManager wm = (WindowManager) this.getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        int screenHeight;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2)
+        {
+            Point size = new Point();
+            display.getSize(size);
+            screenHeight= size.y;
+        }
+        else
+        {
+            screenHeight = display.getWidth();
+        }
+        return screenHeight;
+    }
+
+    private int resizeWidthAccordingToMood(String paramColor)
+    {
+        int newWidth = 0;
+        if(paramColor.equals("#AB1A49"))
+        {
+            newWidth = measureWidth()/5;
+        }
+        if(paramColor.equals("#808A89"))
+        {
+            newWidth = measureWidth()/4;
+        }
+        if(paramColor.equals("#3135D0"))
+        {
+            newWidth = measureWidth()/3;
+        }
+        if(paramColor.equals("#55B617"))
+        {
+            newWidth = measureWidth()/2;
+        }
+        if(paramColor.equals("#D0E807"))
+        {
+            newWidth = measureWidth();
+        }
+        return newWidth;
     }
 }
